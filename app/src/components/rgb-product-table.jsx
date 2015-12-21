b4b13a00cba81adb3197e11cefeb9965d0096bd9
Chris J. Lebron <chris@chrisjlebron.com>
2015-12-20 22:17:06 -0500

    WIP: Sorting rather than culling

--- app/src/components/rgb-product-table.jsx
+++ app/src/components/rgb-product-table.jsx
@@ -3,27 +3,68 @@ import ProductRow from './rgb-product-row';
 
 const ProductTable = ({products, filterText, isLoggedIn, matches}) => {
 
-    let rows = [];
+  let rows = [];
+  let inputRows = products.filter((product) => {
+    let textInName = filterText.length && product.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+    return textInName;
+  });
+  let userRows = products.filter((product) => {
+    return product.userLikes && isLoggedIn;
+  });
+  let resultRows = products.filter((product) => {
+    return inputRows.indexOf(product) === -1 && userRows.indexOf(product) === -1;
+  });
 
-    products.map((product) => {
-      let textInName = product.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+  rows = rows.concat(userRows, inputRows, resultRows).reduce((arr, product) => {
+    arr.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
+    return arr;
+  }, []);
 
-      // if (!textInName || (!product.userLikes && isLoggedIn)) {
-      //     return;
-      // }
+  /*
+    Need to:
+    - only highlight results that match query
+    - also apply query matching sort to userLikes when logged in
+   */
 
-      if (!textInName) {
-        // if product.name in matches, move to top of list...
-        // Otherwise, place after matches
-          return;
-      }
 
-      rows.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
-    });
+  // let rows = products.reduce((arr, product) => {
+  //   let pushit = (newArr) => {
+  //     newArr.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
+  //     return newArr;
+  //   };
+  //
+  //   // if (userRows.indexOf(product) !== -1) {
+  //   //   return pushit(arr);
+  //   // }
+  //
+  //   // if (inputRows.indexOf(product) !== -1) {
+  //   //   return pushit(arr);
+  //   // }
+  //   // arr.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
+  //   return pushit(arr);
+  // }, []);
 
-    return (
-      <ul className="ProductTable">{rows}</ul>
-    );
+  // let rows = products.map((product) => {
+  //   let textInName = product.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+  //   let productRow = <ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>;
+  //
+  //   if (textInName) {
+  //     return productRow;
+  //     // rows.push(productRow);
+  //     // if product.name in matches, move to top of list...
+  //     // Otherwise, place after matches
+  //       // return;
+  //   } else {
+  //     return;
+  //     // rows.push(productRow);
+  //   }
+  //
+  //   // rows.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
+  // });
+
+  return (
+    <ul className="ProductTable">{rows}</ul>
+  );
 }
 
 export default ProductTable;
