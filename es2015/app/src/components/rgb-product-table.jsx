af03e249da2142153a486995273fd68ba81600f9
Chris J. Lebron <chris@chrisjlebron.com>
2015-12-21 13:38:52 -0500

    Correctly sort & transform results array and items

--- es2015/app/src/components/rgb-product-table.jsx
+++ es2015/app/src/components/rgb-product-table.jsx
@@ -1,68 +1,44 @@
 import React from 'react';
+import {findIndex} from 'lodash';
+
 import ProductRow from './rgb-product-row';
 
 const ProductTable = ({products, filterText, isLoggedIn, matches}) => {
 
   let rows = [];
+  // return arry filtered by match with query
   let inputRows = products.filter((product) => {
     let textInName = filterText.length && product.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
     return textInName;
   });
+  // return array filtered by user preferences
   let userRows = products.filter((product) => {
     return product.userLikes && isLoggedIn;
   });
+  // return remaining array
   let resultRows = products.filter((product) => {
-    return inputRows.indexOf(product) === -1 && userRows.indexOf(product) === -1;
+    return findIndex(inputRows, product) === -1 && findIndex(userRows, product) === -1;
   });
 
-  inputRows = inputRows.sort((a, b) => b.userLikes - a.userLikes);
+  // sort matched results by user prefs, if logged in
+  inputRows = inputRows.sort((a, b) => {
+    if (isLoggedIn) return b.userLikes - a.userLikes
+  });
 
-  rows = rows.concat(inputRows, userRows, resultRows).reduce((arr, product) => {
-    arr.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
-    return arr;
-  }, []);
-
-  /*
-    Need to:
-    - also apply query matching sort to userLikes when logged in
-    - avoid "flattenChildren" error
-   */
-
-
-  // let rows = products.reduce((arr, product) => {
-  //   let pushit = (newArr) => {
-  //     newArr.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
-  //     return newArr;
-  //   };
-  //
-  //   // if (userRows.indexOf(product) !== -1) {
-  //   //   return pushit(arr);
-  //   // }
-  //
-  //   // if (inputRows.indexOf(product) !== -1) {
-  //   //   return pushit(arr);
-  //   // }
-  //   // arr.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
-  //   return pushit(arr);
-  // }, []);
-
-  // let rows = products.map((product) => {
-  //   let textInName = product.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
-  //   let productRow = <ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>;
-  //
-  //   if (textInName) {
-  //     return productRow;
-  //     // rows.push(productRow);
-  //     // if product.name in matches, move to top of list...
-  //     // Otherwise, place after matches
-  //       // return;
-  //   } else {
-  //     return;
-  //     // rows.push(productRow);
-  //   }
-  //
-  //   // rows.push(<ProductRow key={product.name} product={product} matches={matches} isLoggedIn={isLoggedIn}/>);
-  // });
+  rows = rows.concat(inputRows, userRows, resultRows)
+    .reduce((newArr, product, index) => {
+      // if product is not in new array, add it, else move on
+      if (findIndex(newArr, product) === -1) newArr.push(product);
+      return newArr;
+    }, [])
+    .map((product) => {
+      return <ProductRow
+        key={product.name}
+        product={product}
+        matches={matches}
+        isLoggedIn={isLoggedIn}
+        />;
+    });
 
   return (
     <ul className="ProductTable">{rows}</ul>
