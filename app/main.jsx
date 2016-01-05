c007054c3524290bb994e954ac18e56974c3c65c
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-05 14:57:37 -0500

    Add routes and app wrapper

--- app/main.jsx
+++ app/main.jsx
@@ -1,14 +1,13 @@
 import './stylesheets/main.css';
 import React from 'react';
 import {render} from 'react-dom';
-import FilterableSearch from './src/components/FilterableSearch/FilterableSearch';
-
+import App from './src/components/App';
 
 function renderShell() {
     var shell = document.createElement('div');
     shell.className = 'app-shell';
     document.body.appendChild(shell);
-    render(<FilterableSearch/>, shell);
+    render(<App/>, shell);
 }
 
 // init shell
---
+++ app/src/components/ColorWheelSearch/ColorWheelSearch.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import ProductList from './../FilterableSearch/ProductList';
+import SearchBar from './../FilterableSearch/SearchBar';
+import Legend from './../FilterableSearch/Legend';
+
+import getFruits from '../../fixtures/colors.js'
+
+class FilterableSearch extends React.Component {
+
+  constructor() {
+    super();
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+
+    this.state = {
+      filterText: '',
+      isLoggedIn: false,
+      matches: []
+    }
+  }
+
+  handleUserInput(filterText, isLoggedIn, matches) {
+    this.setState({
+      filterText: filterText,
+      isLoggedIn: isLoggedIn,
+      matches: matches
+    });
+  }
+
+  render() {
+
+    const products = getFruits();
+
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          isLoggedIn={this.state.isLoggedIn}
+          onUserInput={this.handleUserInput}
+          matches={this.state.matches}
+          products={products}
+        />
+        <Legend/>
+        <ProductList
+          products={products}
+          filterText={this.state.filterText}
+          isLoggedIn={this.state.isLoggedIn}
+          matches={this.state.matches}
+        />
+      </div>
+    );
+  }
+
+}
+
+export default FilterableSearch;
