c0f7842a2a00475a50c683fdf6d718426e4ee9c1
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-07 16:43:14 -0500

    Swap color wheel for Raza's original

--- es2015/app/src/fixtures/colors.js
+++ es2015/app/src/fixtures/colors.js
@@ -2,196 +2,196 @@ export default {
   get() {
     return [
       {
-        name: 'cyan',
-        matches: [
-          { name: 'cyan10', value: 'rgba(0, 255, 255, 1)'},
-          { name: 'cyan8', value: 'rgba(0, 255, 255, .8)'},
-          { name: 'cyan6', value: 'rgba(0, 255, 255, .6)'},
-          { name: 'cyan4', value: 'rgba(0, 255, 255, .4)'},
-          { name: 'cyan2', value: 'rgba(0, 255, 255, .2)'},
-        ],
-        value: 'rgb(0, 255, 255)',
-        related: [
-          'blue-cyan',
-          'cyan-green'
-        ],
-        opposite: 'red'
-      },
-      {
-        name: 'blue-cyan',
-        matches: [
-          {name: 'blue-cyan10', value: 'rgba(0, 124, 255, 1)'},
-          {name: 'blue-cyan8', value: 'rgba(0, 124, 255, .8)'},
-          {name: 'blue-cyan6', value: 'rgba(0, 124, 255, .6)'},
-          {name: 'blue-cyan4', value: 'rgba(0, 124, 255, .4)'},
-          {name: 'blue-cyan2', value: 'rgba(0, 124, 255, .2)'},
-        ],
-        value: 'rgb(0, 124, 255)',
-        related: [
-          'cyan',
-          'blue'
-        ],
-        opposite: 'yellow-red'
-      },
-      {
-        name: 'blue',
-        matches: [
-          {name: 'blue10', value: 'rgba(0, 0, 255, 1)'},
-          {name: 'blue8', value: 'rgba(0, 0, 255, .8)'},
-          {name: 'blue6', value: 'rgba(0, 0, 255, .6)'},
-          {name: 'blue4', value: 'rgba(0, 0, 255, .4)'},
-          {name: 'blue2', value: 'rgba(0, 0, 255, .2)'},
-        ],
-        value: 'rgb(0, 0, 255)',
-        related: [
-          'blue-cyan',
-          'magenta-blue'
-        ],
-        opposite: 'yellow'
-      },
-      {
-        name: 'magenta-blue',
-        matches: [
-          {name: 'magenta-blue10', value: 'rgba(124, 0, 255, 1)'},
-          {name: 'magenta-blue8', value: 'rgba(124, 0, 255, .8)'},
-          {name: 'magenta-blue6', value: 'rgba(124, 0, 255, .6)'},
-          {name: 'magenta-blue4', value: 'rgba(124, 0, 255, .4)'},
-          {name: 'magenta-blue2', value: 'rgba(124, 0, 255, .2)'},
-        ],
-        value: 'rgb(124, 0, 255)',
-        related: [
-          'blue',
-          'magenta'
-        ],
-        opposite: 'green-yellow'
-      },
-      {
-        name: 'magenta',
-        matches: [
-          {name: 'magenta10', value: 'rgba(255, 0, 255, 1)'},
-          {name: 'magenta8', value: 'rgba(255, 0, 255, .8)'},
-          {name: 'magenta6', value: 'rgba(255, 0, 255, .6)'},
-          {name: 'magenta4', value: 'rgba(255, 0, 255, .4)'},
-          {name: 'magenta2', value: 'rgba(255, 0, 255, .2)'},
-        ],
-        value: 'rgb(255, 0, 255)',
-        related: [
-          'magenta-blue',
-          'red-magenta'
-        ],
-        opposite: 'green'
-      },
-      {
-        name: 'red-magenta',
-        matches: [
-          {name: 'red-magenta10', value: 'rgba(255, 0, 124, 1)'},
-          {name: 'red-magenta8', value: 'rgba(255, 0, 124, .8)'},
-          {name: 'red-magenta6', value: 'rgba(255, 0, 124, .6)'},
-          {name: 'red-magenta4', value: 'rgba(255, 0, 124, .4)'},
-          {name: 'red-magenta2', value: 'rgba(255, 0, 124, .2)'},
-        ],
-        value: 'rgb(255, 0, 124)',
-        related: [
-          'magenta',
-          'red'
-        ],
-        opposite: 'cyan-green'
-      },
-      {
         name: 'red',
-        matches: [
-          {name: 'red10', value: 'rgba(255, 0, 0, 1)'},
-          {name: 'red8', value: 'rgba(255, 0, 0, .8)'},
-          {name: 'red6', value: 'rgba(255, 0, 0, .6)'},
-          {name: 'red4', value: 'rgba(255, 0, 0, .4)'},
-          {name: 'red2', value: 'rgba(255, 0, 0, .2)'},
+        tints: [
+          {name: 'red50', value: 'hsl(357, 83%, 50%)'},
+          {name: 'red60', value: 'hsl(357, 83%, 60%)'},
+          {name: 'red70', value: 'hsl(357, 83%, 70%)'},
+          {name: 'red80', value: 'hsl(357, 83%, 80%)'},
+          {name: 'red90', value: 'hsl(357, 83%, 90%)'},
         ],
-        value: 'rgb(255, 0, 0)',
+        value: 'hsl(357, 83%, 50%)',
         related: [
-          'red-magenta',
-          'yellow-red'
-        ],
-        opposite: 'cyan'
-      },
-      {
-        name: 'yellow-red',
-        matches: [
-          {name: 'yellow-red10', value: 'rgba(255, 124, 0, 1)'},
-          {name: 'yellow-red8', value: 'rgba(255, 124, 0, .8)'},
-          {name: 'yellow-red6', value: 'rgba(255, 124, 0, .6)'},
-          {name: 'yellow-red4', value: 'rgba(255, 124, 0, .4)'},
-          {name: 'yellow-red2', value: 'rgba(255, 124, 0, .2)'},
-        ],
-        value: 'rgb(255, 124, 0)',
-        related: [
-          'red',
-          'yellow'
-        ],
-        opposite: 'blue-cyan'
-      },
-      {
-        name: 'yellow',
-        matches: [
-          {name: 'yellow10', value: 'rgba(255, 255, 0, 1)'},
-          {name: 'yellow8', value: 'rgba(255, 255, 0, .8)'},
-          {name: 'yellow6', value: 'rgba(255, 255, 0, .6)'},
-          {name: 'yellow4', value: 'rgba(255, 255, 0, .4)'},
-          {name: 'yellow2', value: 'rgba(255, 255, 0, .2)'},
-        ],
-        value: 'rgb(255, 255, 0)',
-        related: [
-          'yellow-red',
-          'green-yellow'
+          'pink',
+          'orange'
         ],
         opposite: 'blue'
       },
       {
-        name: 'green-yellow',
-        matches: [
-          {name: 'green-yellow10', value: 'rgba(124, 255, 0, 1)'},
-          {name: 'green-yellow8', value: 'rgba(124, 255, 0, .8)'},
-          {name: 'green-yellow6', value: 'rgba(124, 255, 0, .6)'},
-          {name: 'green-yellow4', value: 'rgba(124, 255, 0, .4)'},
-          {name: 'green-yellow2', value: 'rgba(124, 255, 0, .2)'},
+        name: 'orange',
+        tints: [
+          {name: 'orange50', value: 'hsl(35, 94%, 50%)'},
+          {name: 'orange60', value: 'hsl(35, 94%, 60%)'},
+          {name: 'orange70', value: 'hsl(35, 94%, 70%)'},
+          {name: 'orange80', value: 'hsl(35, 94%, 80%)'},
+          {name: 'orange90', value: 'hsl(35, 94%, 90%)'},
         ],
-        value: 'rgb(124, 255, 0)',
+        value: 'hsl(35, 94%, 50%)',
+        related: [
+          'red',
+          'yellow'
+        ],
+        opposite: 'blue-violet'
+      },
+      {
+        name: 'yellow',
+        tints: [
+          {name: 'yellow50', value: 'hsl(57, 88%, 50%)'},
+          {name: 'yellow60', value: 'hsl(57, 88%, 60%)'},
+          {name: 'yellow70', value: 'hsl(57, 88%, 70%)'},
+          {name: 'yellow80', value: 'hsl(57, 88%, 80%)'},
+          {name: 'yellow90', value: 'hsl(57, 88%, 90%)'},
+        ],
+        value: 'hsl(57, 88%, 50%)',
+        related: [
+          'orange',
+          'yellow-green'
+        ],
+        opposite: 'violet'
+      },
+      {
+        name: 'yellow-green',
+        tints: [
+          {name: 'yellow-green50', value: 'hsl(80, 54%, 50%)'},
+          {name: 'yellow-green60', value: 'hsl(80, 54%, 60%)'},
+          {name: 'yellow-green70', value: 'hsl(80, 54%, 70%)'},
+          {name: 'yellow-green80', value: 'hsl(80, 54%, 80%)'},
+          {name: 'yellow-green90', value: 'hsl(80, 54%, 90%)'},
+        ],
+        value: 'hsl(80, 54%, 50%)',
         related: [
           'yellow',
           'green'
         ],
-        opposite: 'magenta-blue'
+        opposite: 'mauve'
       },
       {
         name: 'green',
-        matches: [
-          {name: 'green10', value: 'rgba(0, 255, 0, 1)'},
-          {name: 'green8', value: 'rgba(0, 255, 0, .8)'},
-          {name: 'green6', value: 'rgba(0, 255, 0, .6)'},
-          {name: 'green4', value: 'rgba(0, 255, 0, .4)'},
-          {name: 'green2', value: 'rgba(0, 255, 0, .2)'},
+        tints: [
+          {name: 'green50', value: 'hsl(105, 44%, 50%)'},
+          {name: 'green60', value: 'hsl(105, 44%, 60%)'},
+          {name: 'green70', value: 'hsl(105, 44%, 70%)'},
+          {name: 'green80', value: 'hsl(105, 44%, 80%)'},
+          {name: 'green90', value: 'hsl(105, 44%, 90%)'},
         ],
-        value: 'rgb(0, 255, 0)',
+        value: 'hsl(105, 44%, 50%)',
         related: [
-          'green-yellow',
-          'cyan-green'
+          'yellow-green',
+          'blue-green'
         ],
-        opposite: 'magenta'
+        opposite: 'mauve-pink'
       },
       {
-        name: 'cyan-green',
-        matches: [
-          {name: 'cyan-green10', value: 'rgba(0, 255, 124, 1)'},
-          {name: 'cyan-green8', value: 'rgba(0, 255, 124, .8)'},
-          {name: 'cyan-green6', value: 'rgba(0, 255, 124, .6)'},
-          {name: 'cyan-green4', value: 'rgba(0, 255, 124, .4)'},
-          {name: 'cyan-green2', value: 'rgba(0, 255, 124, .2)'},
+        name: 'blue-green',
+        tints: [
+          {name: 'blue-green50', value: 'hsl(157, 41%, 50%)'},
+          {name: 'blue-green60', value: 'hsl(157, 41%, 60%)'},
+          {name: 'blue-green70', value: 'hsl(157, 41%, 70%)'},
+          {name: 'blue-green80', value: 'hsl(157, 41%, 80%)'},
+          {name: 'blue-green90', value: 'hsl(157, 41%, 90%)'},
         ],
-        value: 'rgb(0, 255, 124)',
+        value: 'hsl(157, 41%, 50%)',
         related: [
-          'cyan',
-          'green'
+          'green',
+          'blue'
         ],
-        opposite: 'red-magenta'
+        opposite: 'pink'
+      },
+      {
+        name: 'blue',
+        tints: [
+          {name: 'blue50', value: 'hsl(198, 78%, 50%)'},
+          {name: 'blue60', value: 'hsl(198, 78%, 60%)'},
+          {name: 'blue70', value: 'hsl(198, 78%, 70%)'},
+          {name: 'blue80', value: 'hsl(198, 78%, 80%)'},
+          {name: 'blue90', value: 'hsl(198, 78%, 90%)'},
+        ],
+        value: 'hsl(198, 78%, 50%)',
+        related: [
+          'blue-green',
+          'blue-violet'
+        ],
+        opposite: 'red'
+      },
+      {
+        name: 'blue-violet',
+        tints: [
+          {name: 'blue-violet50', value: 'hsl(216, 45%, 50%)'},
+          {name: 'blue-violet60', value: 'hsl(216, 45%, 60%)'},
+          {name: 'blue-violet70', value: 'hsl(216, 45%, 70%)'},
+          {name: 'blue-violet80', value: 'hsl(216, 45%, 80%)'},
+          {name: 'blue-violet90', value: 'hsl(216, 45%, 90%)'},
+        ],
+        value: 'hsl(216, 45%, 50%)',
+        related: [
+          'blue',
+          'violet'
+        ],
+        opposite: 'orange'
+      },
+      {
+        name: 'violet',
+        tints: [
+          {name: 'violet50', value: 'hsl(245, 32%, 50%)'},
+          {name: 'violet60', value: 'hsl(245, 32%, 60%)'},
+          {name: 'violet70', value: 'hsl(245, 32%, 70%)'},
+          {name: 'violet80', value: 'hsl(245, 32%, 80%)'},
+          {name: 'violet90', value: 'hsl(245, 32%, 90%)'},
+        ],
+        value: 'hsl(245, 32%, 50%)',
+        related: [
+          'blue-violet',
+          'mauve'
+        ],
+        opposite: 'yellow'
+      },
+      {
+        name: 'mauve',
+        tints: [
+          {name: 'mauve50', value: 'hsl(281, 31%, 50%)'},
+          {name: 'mauve60', value: 'hsl(281, 31%, 60%)'},
+          {name: 'mauve70', value: 'hsl(281, 31%, 70%)'},
+          {name: 'mauve80', value: 'hsl(281, 31%, 80%)'},
+          {name: 'mauve90', value: 'hsl(281, 31%, 90%)'},
+        ],
+        value: 'hsl(281, 31%, 50%)',
+        related: [
+          'violet',
+          'mauve-pink'
+        ],
+        opposite: 'yellow-green'
+      },
+      {
+        name: 'mauve-pink',
+        tints: [
+          {name: 'mauve-pink50', value: 'hsl(324, 58%, 50%)'},
+          {name: 'mauve-pink60', value: 'hsl(324, 58%, 60%)'},
+          {name: 'mauve-pink70', value: 'hsl(324, 58%, 70%)'},
+          {name: 'mauve-pink80', value: 'hsl(324, 58%, 80%)'},
+          {name: 'mauve-pink90', value: 'hsl(324, 58%, 90%)'},
+        ],
+        value: 'hsl(324, 58%, 50%)',
+        related: [
+          'mauve',
+          'pink'
+        ],
+        opposite: 'green'
+      },
+      {
+        name: 'pink',
+        tints: [
+          {name: 'pink50', value: 'hsl(333, 82%, 50%)'},
+          {name: 'pink60', value: 'hsl(333, 82%, 60%)'},
+          {name: 'pink70', value: 'hsl(333, 82%, 70%)'},
+          {name: 'pink80', value: 'hsl(333, 82%, 80%)'},
+          {name: 'pink90', value: 'hsl(333, 82%, 90%)'},
+        ],
+        value: 'hsl(333, 82%, 50%)',
+        related: [
+          'mauve-pink',
+          'red'
+        ],
+        opposite: 'blue-green'
       }
     ]
   }
