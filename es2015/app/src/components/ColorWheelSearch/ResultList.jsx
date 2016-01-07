1afb95bc52931efa69d279e8bc8efb438b93846a
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-07 17:22:32 -0500

    Update default result list handling to show tints

--- es2015/app/src/components/ColorWheelSearch/ResultList.jsx
+++ es2015/app/src/components/ColorWheelSearch/ResultList.jsx
@@ -5,13 +5,12 @@ import ListItem from './ListItem';
 import {findIndex} from 'lodash';
 
 const ResultList = ({results, filterText}) => {
-  let listClass = '';
   let listItems = results.map((result) => {
     let row;
-    let match;
     let related;
     let opposite;
     let oppositeRelated;
+    let key = result.name;
     let filterRelated = (outerArray, matchObject) => {
       return outerArray.filter((relatedResult) => {
         return matchObject.related.indexOf(relatedResult.name) !== -1;
@@ -20,45 +19,43 @@ const ResultList = ({results, filterText}) => {
 
     if (filterText.length) {
       // // filterText contains result.name
-      match = result.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1 ? result : undefined;
+      // let match = result.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1 ? result : undefined;
 
       // // filterText equals result.name
-      // match = result.name.toLowerCase() === filterText.toLowerCase() ? result : undefined;
-      listClass = 'ResultList';
+      let match = result.name.toLowerCase() === filterText.toLowerCase() ? result : undefined;
+
+      if (match) {
+        opposite = results.filter((oppResult) => {
+          return oppResult.name === match.opposite;
+        });
+        related = filterRelated(results, match);
+        oppositeRelated = filterRelated(results, opposite[0]);
+        row = [
+          {title: 'Match', results: [match]},
+          {title: 'Match\'s Neighbors', results: related},
+          {title: 'Opposite', results: opposite},
+          {title: 'Opposite\'s Neighbors', results: oppositeRelated}
+        ];
+      } else {
+        // user input doesn't match any results
+        return false;
+      }
     } else {
-      listClass = 'result-list';
-        return (
-          <ListItem
-            key={result.name}
-            result={result}
-          />
-        );
-    }
-
-    if (typeof match === 'object') {
-      opposite = results.filter((oppResult) => {
-        return oppResult.name === match.opposite;
-      });
-      related = filterRelated(results, match);
-      oppositeRelated = filterRelated(results, opposite[0]);
       row = [
-        {title: 'Match', results: [match]},
-        {title: 'Match\'s Neighbors', results: related},
-        {title: 'Opposite', results: opposite},
-        {title: 'Opposite\'s Neighbors', results: oppositeRelated}
-      ];
-
-      return (
-        <ResultRow
-          key={match.name}
-          row={row}
-        />
-      );
+        {title: `${result.name}s`, results: result.tints}
+      ]
     }
+
+    return (
+      <ResultRow
+        key={key}
+        row={row}
+      />
+    );
   });
 
   return (
-    <ul className={listClass}>{listItems}</ul>
+    <ul className="ResultList">{listItems}</ul>
   );
 }
 
