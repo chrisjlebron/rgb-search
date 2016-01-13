c0c673434b7784843b4314ff0a5c4feb3da7b3cc
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-13 16:08:26 -0500

    WIP: Refactoring major components; ColorWheelSearch should be good
    WIP WIP: fucking refactor

---
+++ es2015/app/src/components/ColorWheelSearch/ResultDisambiguation.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import ResultRow from './ResultRow';
+
+import {capitalize} from 'lodash';
+
+const ResultDisambiguation = ({results, filterText, categories, onUserClick}) => {
+  if (!filterText.length) {
+    return {
+      render() {
+          return null;
+      }
+    }
+  }
+
+  // let mapResults = () => {
+  //   return results.map((result) => {
+  //     let row;
+  //     let key;
+  //
+  //     // filterText equals result.name
+  //     let match = result.name.toLowerCase() === filterText.toLowerCase() ? result : undefined;
+  //
+  //     if (match) {
+  //       let category = categories.filter((cat) => cat.name === match.category)[0];
+  //       let matches = results
+  //         .filter((res) => res.category === category.name)
+  //         .sort((a, b) => {
+  //           if (a.name === filterText) return -1;
+  //           if (b.name === filterText) return 1;
+  //           return 0;
+  //         });
+  //       let related = results.filter((res) => category.related.indexOf(res.name) !== -1);
+  //       let opposite = results.filter((res) => res.name === category.opposite)[0];
+  //       let oppCategory = categories.filter((cat) => cat.name === opposite.category)[0];
+  //       let oppositeRelated = results.filter((res) => oppCategory.related.indexOf(res.name) !== -1);
+  //
+  //       key = match.name;
+  //       row = [
+  //         {title: 'Match\'s Neighbors', results: related},
+  //         {title: 'Opposite', results: [opposite]},
+  //         {title: 'Opposite\'s Neighbors', results: oppositeRelated},
+  //         {title: `Showing ${capitalize(category.name)}'s`, results: matches}
+  //       ];
+  //     } else {
+  //       // // user input doesn't match any results
+  //       return false;
+  //     }
+  //
+  //     return (
+  //       <ResultRow
+  //         key=`${key}-disambiguation`
+  //         row={row}
+  //         onUserClick={onUserClick}
+  //       />
+  //     );
+  //   });
+  // };
+  //
+  // let disambiguationGroup = [(
+  //     // no user input is default
+  //     <ResultRow
+  //       key={'all'}
+  //       row={[{title: 'Available Data', results: results}]}
+  //       onUserClick={onUserClick}
+  //     />
+  // )];
+  //
+  // if (filterText.length) {
+  //   disambiguationGroup = mapResults();
+  // }
+
+  return (
+    <ul className="ResultDisambiguation">{disambiguationGroup}</ul>
+  );
+}
+
+export default ResultDisambiguation;
