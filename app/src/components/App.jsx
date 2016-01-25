133ea84a8b9c1267d449088898cc88eec2bb2ac9
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-25 15:16:16 -0500

    Add react router and routes

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,62 +1,26 @@
 import '../../stylesheets/main.scss';
 import React from 'react';
-
-import Home from './Home';
-import FilterableSearch from '../../src/components/FilterableSearch/FilterableSearch';
-import ColorWheelSearch from '../../src/components/ColorWheelSearch/ColorWheelSearch';
-
+import {Link, IndexLink} from 'react-router';
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      route: window.location.hash.substr(1)
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({
-        route: window.location.hash.substr(1)
-      })
-    })
-  }
-
   render() {
-    let Child;
-    let activeClass = 'active';
-    const HomeRoute = '/';
-    const FSRoute = '/filterable-search';
-    const CWRoute = '/color-wheel-search';
-
-    switch (this.state.route) {
-      case FSRoute:
-        Child = FilterableSearch;
-        break;
-      case CWRoute:
-        Child = ColorWheelSearch;
-        break;
-      case HomeRoute: Child = Home;
-      default: Child = Home;
-    }
-
+    let pathname = this.props.location.pathname;
 
     return (
       <div className="App">
         <h1>{'RGB Search Prototypes'}</h1>
         <ul className="nav">
-          <li className={this.state.route === HomeRoute ? 'active' : ''}>
-            <a href="#/">{'Home'}</a>
+          <li className={pathname === '/' ? 'active' : ''}>
+            <IndexLink to={'/'}>{'Home'}</IndexLink>
           </li>
-          <li className={this.state.route === FSRoute ? 'active' : ''}>
-            <a href="#/filterable-search">{'Filterable Search'}</a>
+          <li className={pathname === '/filterable-search' ? 'active' : ''}>
+            <Link to={'/filterable-search'}>{'Filterable Search'}</Link>
           </li>
-          <li className={this.state.route === CWRoute ? 'active' : ''}>
-            <a href="#/color-wheel-search">{'Color Wheel Search'}</a>
+          <li className={pathname === '/color-wheel-search' ? 'active' : ''}>
+            <Link to={'/color-wheel-search'}>{'Color Wheel Search'}</Link>
           </li>
         </ul>
-        <Child/>
+        {this.props.children}
       </div>
     )
   }
