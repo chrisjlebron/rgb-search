ddfbefd521f1bd626ba36539fcf6362c945c8310
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-29 18:20:49 -0500

    Add more data to results

--- app/src/components/ChatSearch/ProfileResult.jsx
+++ app/src/components/ChatSearch/ProfileResult.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const ProfileResult = ({result}) => {
   return (
     <li className="ProfileResult">
-      {result.title}
+      <div className="profile-title"><b>Title:</b> {result.title}</div>
+      <div className="profile-type"><b>Type:</b> {result.type}</div>
+      <div className="profile-size"><b>Size:</b> {result.size}</div>
+      <div className="profile-setting"><b>Setting:</b> {result.setting}</div>
+      <div className="profile-gpa_cutoff"><b>GPA Cutoff:</b> {result.gpa_cutoff.toFixed(1)}</div>
+      <div className="profile-tuition_cost"><b>Tuition Cost:</b> {result.tuition_cost}</div>
+      <div className="profile-region"><b>Region:</b> {result.region}</div>
     </li>
   );
 };
