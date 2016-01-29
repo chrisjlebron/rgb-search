0fcf99d7395a278bec6cc805b34210a242e75a4c
Chris J. Lebron <chris@chrisjlebron.com>
2016-01-29 18:18:47 -0500

    WIP: add some questions to a fixture

---
+++ app/src/fixtures/questions.js
@@ -0,0 +1,99 @@
+// 'type': 'schools',
+// 'title': 'NYU',
+// 'size': 22615,
+// 'setting': 'urban',
+// 'gpa_cutoff': 3.0,
+// 'region': 'mid-atlantic',
+// 'tuition_cost': 42472
+
+export default {
+  get() {
+    return [
+      {
+        "id": 0,
+        "preference": "size",
+        "text": "what size school would you prefer",
+        "options": [
+          {
+            "id": 0,
+            "text": "whatevs",
+          },
+          {
+            "id": 1,
+            "text": "big",
+          },
+          {
+            "id": 2,
+            "text": "small",
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "preference": "social life",
+        "text": "how important is having a social life",
+        "options": [
+          {
+            "id": 0,
+            "text": "whatevs",
+          },
+          {
+            "id": 1,
+            "text": "Nope. I'm here to study",
+          },
+          {
+            "id": 2,
+            "text": "Board games & my fam; that's all I need",
+          },
+          {
+            "id": 3,
+            "text": "When the uppers get too strong, I switch to downers",
+          },
+        ]
+      },
+      {
+        "id": 2,
+        "preference": "greek life",
+        "text": "How important is greek life",
+        "options": [
+          {
+            "id": 0,
+            "text": "whatevs",
+          },
+          {
+            "id": 1,
+            "text": "No. Fucking. Way.",
+          },
+          {
+            "id": 2,
+            "text": "I'm cool with it",
+          },
+          {
+            "id": 3,
+            "text": "Greek life, all the way. OBVI!",
+          },
+        ]
+      },
+      {
+        "id": 3,
+        "preference": "tuition_cost",
+        "text": "about how much are you hoping to pay for tuition"
+      },
+      {
+        "id": 4,
+        "preference": "setting",
+        "text": "in which setting are you looking to study"
+      },
+      {
+        "id": 5,
+        "preference": "region",
+        "text": "in which region are you looking to study"
+      },
+      {
+        "id": 6,
+        "preference": "gpa_cutoff",
+        "text": "what's your GPA"
+      }
+    ]
+  }
+}
